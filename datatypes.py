2c98097c8600a968bfa6e48c78e8d62d130e68c3
zaidkamil <xaidmetamorphos@gmail.com>
2022-07-31 14:04:52 +0530

    basics

---
+++ datatypes.py
@@ -0,0 +1,9 @@
+x = 1920
+y = 9203
+x2 = -39021309
+y2 = -9203
+print(x , type(x))
+print(y , type(y))
+print(x2 , type(x2))
+print(y2 , type(y2))
+# comment
---
+++ operators.py
@@ -0,0 +1,18 @@
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b) # float division
+print(a // b) # integer division (quotient)
+print(a % b) # remainder
+print(a ** b) # exponent
+
+# operator precedence
+# https://docs.python.org/3/reference/expressions.html#operator-precedence
+
+fruits = ['Apple', 'Banana', 'Orange', 'Pear', 'Grape']
+print('papaya' in fruits)
+print('apple' in fruits)
+print('Apple' in fruits)
